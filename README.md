7807c40e0d2b322985d1a918c62aaca62bef1d5e
feselene <vaporflyb@gmail.com>
2024-09-08 13:18:02 -0400

    fix .env folder in wrong location. delete deprecated options from mongoose.connect

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 1. To run, clone the project. 
-2. cd uber-be
+2. cd anylove-be
 3. npm install
 4. node src/app.js
--- src/app.js
+++ src/app.js
@@ -1,18 +1,7 @@
-const express = require('express'); // Import Express
+const express = require('express');
 const connectDB = require('./config/db');
-const mongoose = require('mongoose'); // Import Mongoose to interact with MongoDB
-const dotenv = require('dotenv'); // Import dotenv to load environment variables
-const PhoneNumber = require('./models/PhoneNumber'); // Import the PhoneNumber model
-
-// Load environment variables from .env file
-// dotenv.config(); // This makes sure process.env.MONGO_URI and process.env.PORT are available
-require('dotenv').config();
-
-// Initialize the Express application
 const app = express();
-
-// Use JSON middleware to parse incoming requests with JSON payloads
-app.use(express.json())
+require('dotenv').config();
 
 // Connect Database
 connectDB();
@@ -23,21 +12,6 @@ app.use(express.json({ extended: false }));
 // Define Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/profile', require('./routes/profileRoutes'));
-app.use('/api/phone', require('./routes/phoneNumberRoutes'));
 
-// Connect to MongoDB using the MONGO_URI from .env
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-    .then(() => {
-      console.log('MongoDB connected successfully');
-      
-      // Start the server after successful MongoDB connection
-      const PORT = process.env.PORT || 5000;
-      app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-    })
-    .catch((err) => {
-      console.error('MongoDB connection error:', err);
-    });
-  
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
